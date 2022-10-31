dd325d5e774c4cfda23c4524c2ac8e1ee189c13b
Ily1chh <ilyakuznetsov99@mail.ru>
2022-10-31 21:34:28 +0300

    red hw

--- homework/hw_UN8/game_hw_UN8.py
+++ homework/hw_UN8/game_hw_UN8.py
@@ -5,7 +5,7 @@ def random_predict(number:int = np.random.randint(1, 101)) -> int:
     count = 0
     max_number = 100
     min_number = 0
-    predict_number = np.random.randint(1, 101) # prospective number
+    predict_number = np.random.randint(1, 101)
  
     while True:
        count += 1
